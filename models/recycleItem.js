ef4d5873448c206465513f5a922e845d5cdcb65f
aditya-075 <146202236+aditya-075@users.noreply.github.com>
2024-09-08 22:46:05 +0530

    Update recycleItem.js

--- models/recycleItem.js
+++ models/recycleItem.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(`mongodb://localhost:27017/hackathonTest2`);
+mongoose.connect(`mongodb://localhost:27017/SmartBin`);
 
 const recycleItemSchema = mongoose.Schema({
     user : {
